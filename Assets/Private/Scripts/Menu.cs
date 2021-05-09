9d45013739ba41611842049d1c9b8202798b5a28
UMA <ume0kanaz@gmail.com>
2021-05-09 22:37:34 +0900

    add UI

---
+++ Assets/Private/Scripts/Menu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] private Button openDoorButton;
+    [SerializeField] private DoorController doorController;
+    [SerializeField] private InputField inputField;
+
+    private void Start()
+    {
+        openDoorButton.onClick.AddListener(ToggleDoor);
+        inputField = inputField.GetComponent<InputField>();
+    }
+
+    private void ToggleDoor()
+    {
+        Debug.Log(inputField.text);
+
+        if (inputField.text.Equals("5555"))
+        {
+            doorController.open();
+        }
+        else
+        {
+            doorController.close();
+        }
+        
+    }
+}
