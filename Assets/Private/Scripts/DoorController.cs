226f0f19188ffed9f9264da47b9718da7e42c195
UMA <ume0kanaz@gmail.com>
2021-05-09 11:20:07 +0900

    add a door and a button

---
+++ Assets/Private/Scripts/DoorController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class DoorController : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private bool coroutineOpenAndClose;
+    private bool isOpen = false;
+    private bool isNearbyPlayer = false;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        if (coroutineOpenAndClose) StartCoroutine(DoorOpenAndClose());
+    }
+
+    private IEnumerator DoorOpenAndClose()
+    {
+        while (true)
+        {
+            if (isNearbyPlayer)
+            {
+                isOpen = true;
+            }
+            else
+            {
+                if (isOpen)
+                {
+                    animator.SetBool("character_nearby", false);
+                    isOpen = false;
+                }
+                else
+                {
+                    animator.SetBool("character_nearby", true);
+                    isOpen = true;
+                }
+            }
+            yield return new WaitForSeconds(3);
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isNearbyPlayer = true;
+            open();
+        } 
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isNearbyPlayer = false;
+            close();
+        }
+    }
+
+
+    public void open()
+    {
+        isOpen = true;
+        animator.SetBool("character_nearby", true);
+    }
+
+
+    public void close()
+    {
+        isOpen = false;
+        animator.SetBool("character_nearby", false);
+    }
+}
---
+++ Assets/Private/Scripts/PushButton.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+[RequireComponent(typeof(Collider))]
+public class PushButton : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private DoorController doorController;
+    private bool isPush = false;
+    private bool isEnter = false;
+    
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        //StartCoroutine(ButtonPushAndBack());
+    }
+
+
+    private void Update()
+    {
+        if (isEnter && CrossPlatformInputManager.GetButtonDown("push"))
+        {
+            if (isPush)
+            {
+                animator.SetBool("try_to_push", false);
+                doorController.close();
+                isPush = false;
+            }
+            else
+            {
+                animator.SetBool("try_to_push", true);
+                doorController.open();
+                isPush = true;
+            }
+            new WaitForSeconds(1);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Enter");
+        isEnter = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Exit");
+        isEnter = false;
+    }
+
+}
