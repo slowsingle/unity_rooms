10256627a2075a73c9ac9978992aa550b4e0f58c
UMA <ume0kanaz@gmail.com>
2021-06-06 17:13:49 +0900

    add color controll script

---
+++ Assets/Private/Scripts/Calc2Color.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calc2Color
+{
+    public static List<string> keyList = new List<string>()
+    {
+        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "x", "="
+    };
+
+    public static List<Color> colorList = new List<Color>()
+    {
+        new Color(0f, 0f, 0f, 1f),  //"black"
+        new Color(0.83f, 0.68f, 0f, 1f),  //"yellow"
+        new Color(0f, 0f, 0f, 0.5f),  //"none"
+        new Color(1f, 0.63f, 0.95f, 1f),  //"pink"
+        new Color(1f, 0f, 0f, 1f),  //"red"
+        new Color(0.13f, 0.7f, 0f, 1f),  //"green"
+        new Color(0f, 0f, 0f, 0.5f),  //"none"
+        new Color(0f, 0.3f, 1.0f, 1f),  //"blue"
+        new Color(0.45f, 0.26f, 0.16f, 1f),  //"brown"
+        new Color(1f, 0.46f, 0f, 1f),  //"orange"
+        new Color(1f, 1f, 1f, 1f),  //"white"
+        new Color(0.5f, 0f, 0.8f, 1f),  //"purple"
+        new Color(0.45f, 0.45f, 0.45f, 1f),  //"gray"
+    };
+
+    public static Color getColorValue(string key)
+    {
+        for (int i = 0; i < colorList.Count; i++)
+        {
+            if (keyList[i] == key)
+            {
+                if (key == "2" || key == "6")
+                {
+                    Debug.LogError("key " + key + " is not implemented");
+                }
+
+                return colorList[i];
+            }
+        }
+
+        Debug.Log("not found color");
+        return new Color(0f, 0f, 0f, 0.5f);
+    }
+
+}
--- Assets/Private/Scripts/doorOpenPanel.cs
+++ Assets/Private/Scripts/doorOpenPanel.cs
@@ -12,19 +12,31 @@ public class doorOpenPanel : MonoBehaviour
     [SerializeField] private InputField inputField;
     [SerializeField] private Button enterButton;
     [SerializeField] private DoorController doorController;
-    [SerializeField] private string password;
+    [SerializeField] private string question;
+    [SerializeField] private string answer;
+    [SerializeField] private List<Image> imageList;
 
     private void Start()
     {
         enterButton.onClick.AddListener(checkPassword);
         inputField = inputField.GetComponent<InputField>();
+
+        if (question.Length != imageList.Count)
+        {
+            Debug.LogError("question.Length != imageList.Count");
+        }
+
+        for (int i = 0; i < imageList.Count; i++)
+        {
+            imageList[i].color = Calc2Color.getColorValue(question[i].ToString());
+        }
     }
 
     private void checkPassword()
     {
         Debug.Log(inputField.text);
 
-        if (inputField.text.Equals(password))
+        if (inputField.text.Equals(answer))
         {
             doorController.open();
         }
